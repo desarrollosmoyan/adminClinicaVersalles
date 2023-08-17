06c279f93e59637685f7112b0b448ab712ccf91b
jose david <81387297+josedavid1996@users.noreply.github.com>
2023-08-17 14:02:57 -0500

    Se agrego nuevos campos a la tabla de pedidos en el perfil

--- src/views/pages/perfil/UserTask.tsx
+++ src/views/pages/perfil/UserTask.tsx
@@ -5,35 +5,165 @@ import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
 import Typography from '@mui/material/Typography'
-import CardContent from '@mui/material/CardContent'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
 
 // ** Graphql
 import { PedidoEntity } from 'src/generated/graphql'
 
 // ** Moment Imports
-import moment from 'moment'
+
+import { Divider } from '@mui/material'
 
 interface Props {
   task: PedidoEntity[]
 }
-
+interface CellType {
+  row: any
+}
 const UserTask = ({ task }: Props) => {
   console.log(task)
+
+  // ** Columns
+  const columns: GridColDef[] = [
+    {
+      flex: 0.25,
+      minWidth: 280,
+      field: 'cliente',
+      headerName: 'Cliente',
+      renderCell: ({ row }: CellType) => {
+        return (
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            {/* {renderClient(row)} */}
+            <Box sx={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'column' }}>
+              <Typography noWrap sx={{ color: 'text.secondary', textTransform: 'capitalize' }}>
+                {row.attributes.cliente}
+              </Typography>
+            </Box>
+          </Box>
+        )
+      }
+    },
+
+    {
+      flex: 0.15,
+      field: 'tipoIdentificacion',
+      minWidth: 170,
+      headerName: 'Tipo de Identificacion',
+      renderCell: ({ row }: CellType) => {
+        return (
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography noWrap sx={{ color: 'text.secondary', textTransform: 'capitalize' }}>
+              {row.attributes.tipoIdentificacion || ''}
+            </Typography>
+          </Box>
+        )
+      }
+    },
+    {
+      flex: 0.15,
+      field: 'identidicacion',
+      minWidth: 170,
+      headerName: 'Identificacion',
+      renderCell: ({ row }: CellType) => {
+        return (
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography noWrap sx={{ color: 'text.secondary', textTransform: 'capitalize' }}>
+              {row.attributes.identificacion || ''}
+            </Typography>
+          </Box>
+        )
+      }
+    },
+    {
+      flex: 0.15,
+      field: 'descripcion',
+      minWidth: 170,
+      headerName: 'Descripcion',
+      renderCell: ({ row }: CellType) => {
+        return (
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography noWrap sx={{ color: 'text.secondary', textTransform: 'capitalize' }}>
+              {row.attributes.descripcion}
+            </Typography>
+          </Box>
+        )
+      }
+    },
+    {
+      flex: 0.15,
+      field: 'estacionInicio',
+      minWidth: 170,
+      headerName: 'Estacion Inicio',
+      renderCell: ({ row }: CellType) => {
+        return (
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography noWrap sx={{ color: 'text.secondary', textTransform: 'capitalize' }}>
+              {row.attributes.estacionInicio}
+            </Typography>
+          </Box>
+        )
+      }
+    },
+    {
+      flex: 0.15,
+      field: 'estacionFin',
+      minWidth: 170,
+      headerName: 'Estacion Fin',
+      renderCell: ({ row }: CellType) => {
+        return (
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography noWrap sx={{ color: 'text.secondary', textTransform: 'capitalize' }}>
+              {row.attributes.estacionFin}
+            </Typography>
+          </Box>
+        )
+      }
+    }
+  ]
 
   return (
     <>
       <Grid container spacing={12}>
-        {task && task.length !== 0 ? (
+        <Grid item xs={12}>
+          {task !== undefined && task.length !== 0 && (
+            <Card>
+              <Divider sx={{ m: '0 !important' }} />
+              {/* <TableHeader
+              value={value}
+              handleFilter={handleFilter}
+              toggle={toggleAddUserDrawer}
+              name='Agregar Pedido'
+              nameSearch='Buscar pedidos'
+              data={dataExport}
+            /> */}
+              <DataGrid
+                autoHeight
+                rowHeight={62}
+                rows={task}
+                columns={columns}
+                disableRowSelectionOnClick
+                pageSizeOptions={[10, 25, 50]}
+                localeText={{ noRowsLabel: 'No hay información' }}
+                componentsProps={{
+                  pagination: {
+                    labelRowsPerPage: 'Filas por página'
+                  }
+                }}
+              />
+            </Card>
+          )}
+        </Grid>
+        {/* {task && task.length !== 0 ? (
           task.map(item => {
             return (
               <Grid key={item.id} item xs={12} md={6} lg={4}>
                 <Card>
                   <CardContent>
-                    {/* <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
                       <Box sx={{ display: 'flex', alignItems: 'center' }}>
                         <Typography variant='h5'>{item.attributes?.nombrePedido}</Typography>
                       </Box>
-                    </Box> */}
+                    </Box>
                     <Typography sx={{ my: 4, color: 'text.secondary' }}>{item.attributes?.descripcion}</Typography>
                     <Box sx={{ gap: 2, display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}></Box>
                     <Box
@@ -83,7 +213,7 @@ const UserTask = ({ task }: Props) => {
           <Grid item xs={12}>
             <Typography variant='h2'>No hay tareas</Typography>
           </Grid>
-        )}
+        )} */}
       </Grid>
     </>
   )
