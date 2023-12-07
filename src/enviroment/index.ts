eba338db7db9ee7f05b0a5b0c5dd208481b20533
Cristian Gonzalez <kazragore96@gmail.com>
2023-12-06 19:55:57 -0500

    feat: Agregado .env

--- src/enviroment/index.ts
+++ src/enviroment/index.ts
@@ -1,5 +1,5 @@
 const URL_DEV_LOCAL = 'http://192.168.1.18:1337'
-const URL_PRD = 'https://c4a1-190-66-99-88.ngrok-free.app'
+const URL_PRD = process.env.NEXT_PUBLIC_URL ?? ''
 
 const ENV = {
   DEV_LOCAL: { URL: URL_DEV_LOCAL },
